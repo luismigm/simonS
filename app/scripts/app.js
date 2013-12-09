7407baee4a8d52f69aa4a8d9f100e2963048fe15
luismigm <luismigm@hotmail.com>
2013-12-09 06:44:15 -0500

    divs simon, eventos hover y mouseleave

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,7 +3,46 @@ define([], function () {
     'use strict';
     $(document).ready(function()
    	{
-    	alert("Bienvenido a Simon Says");
-   	})
+    	$("#rojo").hover(function()
+    		{
+		    	$(this).css("background-color","#f84f32");
+
+		   	});
+    	$("#rojo").mouseleave(function()
+    		{
+		    	$(this).css("background-color","red");
+
+		   	});
+    	$("#verde").hover(function()
+    		{
+		    	$(this).css("background-color","#91e842");
+
+		   	});
+    	$("#verde").mouseleave(function()
+    		{
+		    	$(this).css("background-color","green");
+
+		   	});
+    	$("#azul").hover(function()
+    		{
+		    	$(this).css("background-color","#207ce5");
+
+		   	});
+    	$("#azul").mouseleave(function()
+    		{
+		    	$(this).css("background-color","blue");
+
+		   	});
+    	$("#amarillo").hover(function()
+    		{
+		    	$(this).css("background-color","#feb645");
+
+		   	});
+    	$("#amarillo").mouseleave(function()
+    		{
+		    	$(this).css("background-color","yellow");
+
+		   	});
+   	});
     return 0;
 });
